62d5c5e94aa0717552c761cce3e3dd770f7c08be
alameen <alameenraji31@gmail.com>
2020-09-03 22:26:45 +0100

    rajman

--- language/views.py
+++ language/views.py
@@ -57,8 +57,8 @@ def analyze(request, pk):
             try:
                 q = request.POST[str(question.id)]
             except MultiValueDictKeyError:
-                messages.danger(request, f'Select a choice for all questions.')
-                return redirect('analyze', pk=pk)
+                messages.warning(request, f'Select a choice for all questions.')
+                return redirect('study', pk=pk)
                 # return render(request, 'language/study_material.html',
                 #               context={'material': material, 'error': 'Select a choice for all questions'})
             answers.append(q)
